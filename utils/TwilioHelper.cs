1b78d0a4060361043aa99328cb2a15e7dea09fc1
manue873 <945655309cell@gmail.com>
2024-10-30 00:38:00 -0500

    Manuel:error

--- utils/TwilioHelper.cs
+++ utils/TwilioHelper.cs
@@ -6,19 +6,20 @@ namespace WEBAPP_NATURPIURA.utils
 {
     public class TwilioHelper
     {
-        private SMSSetings smsSetings = new SMSSetings();
-        public string sendSMSMEssage(string toMobileNumber, string messagetosend)
-        {
-            TwilioClient.Init(
-                smsSetings.Twilio_Account_SID,
-                smsSetings.Twilio_Auth_TOKEN
-                );
-            var message = MessageResource.Create(
-                from: new PhoneNumber(smsSetings.Twilio_Phone_Number),
-                to: new PhoneNumber("+51" + toMobileNumber),
-                body: messagetosend
-                );
-            return message.Sid;
-        }
+        //debes crear la entidad con tokens 
+        //private SMSSetings smsSetings = new SMSSetings();
+        //public string sendSMSMEssage(string toMobileNumber, string messagetosend)
+        //{
+        //    TwilioClient.Init(
+        //        smsSetings.Twilio_Account_SID,
+        //        smsSetings.Twilio_Auth_TOKEN
+        //        );
+        //    var message = MessageResource.Create(
+        //        from: new PhoneNumber(smsSetings.Twilio_Phone_Number),
+        //        to: new PhoneNumber("+51" + toMobileNumber),
+        //        body: messagetosend
+        //        );
+        //    return message.Sid;
+        //}
     }
 }
